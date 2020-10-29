57c4f7dcaba1e27b65de3dc0b1c7d7f5742d6fb9
youssefelya <youssefelyamani96@gmail.com>
2020-10-29 19:35:59 +0100

    first commit, setting up the project and creating card for the view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'github-repo';
+  repos=[];
+  testObject = {
+    full_name:'Youssef ELYA',
+    name:'Youssef',
+    owner:{
+      login:'Login',
+      avatar_url:'https://avatars.githubusercontent.com/u/583231?v=3',
+    }
+  }
+  constructor(){
+    this.repos=Array(30).fill(this.testObject);
+  }
 }
